852617d93fb2a895e09e94934f0076f474e190ee
just <Virjunlargo6@Gmail.com>
2025-08-30 22:46:53 +0800

    Set DEBUG=True to diagnose 500 error

--- apartment/settings.py
+++ apartment/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-6_iezrvrparvw$epkyvl**trt2l0*$y!dt2&u+9$#gm0u0t5h&')
-DEBUG = False
+DEBUG = True  # Changed to True for debugging
 ALLOWED_HOSTS = ['apartment-p51r.onrender.com', '127.0.0.1', 'localhost']
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -52,7 +52,7 @@ else:
         'default': dj_database_url.config(conn_max_age=600, ssl_require=True, default='sqlite:///db.sqlite3')
     }
 STATIC_URL = '/static/'
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'accounts', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
